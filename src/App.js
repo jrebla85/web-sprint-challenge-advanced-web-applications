be53a59ebaa46ab9192520be0617cb1b676981d0
jeremy black <jrebla85@yahoo.com>
2021-07-17 20:57:29 -0500

    finished index.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
+
 import Login from "./components/Login";
+import BubblePage from "./components/BubblePage";
 import "./styles.scss";
 
 function App() {
+  
+  const logout = () => {
+    localStorage.removeItem("token");
+  }
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">logout</a>
+          <a onClick={logout} data-testid="logoutButton" href="http://localhost:3000/">logout</a>
         </header> 
-
+        <PrivateRoute exact path="/bubblepage" component={BubblePage}/>
         <Route exact path="/" component={Login} />
       </div>
     </Router>
@@ -20,7 +28,3 @@ function App() {
 }
 
 export default App;
-
-//Task List:
-//1. Render BubblePage as a PrivateRoute
-//2. Build the logout button to call the logout endpoint, remove the localStorage Item and redirect to the login page.
