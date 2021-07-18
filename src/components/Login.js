9fbeb51a20b11712fd5d811acc55727f73fb7e46
jeremy black <jrebla85@yahoo.com>
2021-07-18 01:29:15 -0500

    Got colors and bubbles to display

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import React, { sideEffect, useState } from "react";
 import { axiosWithAuth } from '../helpers/axiosWithAuth'
+import { useHistory } from 'react-router-dom'
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
   const [form, setForm] = useState({username: "", password: ""})
-  
+  const {push} = useHistory()  
   const handleChange = (e) => {
     setForm({
       ...form,
@@ -19,7 +20,7 @@ const Login = () => {
     axiosWithAuth().post('/login', form)
       .then(res => {
         localStorage.setItem('token', res.data.payload)
-        console.log(res)
+        push('/bubbles')
       })
       .catch(err => console.log(err))
   }
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -9,7 +9,7 @@ export const PrivateRoute = (props) => {
             if(localStorage.getItem('token')){
                 return <Component {...renderProps} />
             } else {
-                return <Redirect to="/login" />
+                return <Redirect to="/" />
             }
         }}/>
     )
